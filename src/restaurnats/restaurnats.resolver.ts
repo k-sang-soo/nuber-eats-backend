a33f6c3081334224ea750e62947d31031a8c30a1
spadecompany-sangsoo <sangsoo@spadecomapny.kr>
2023-06-19 18:47:19 +0900

    graphql args 추가 하는 방법

--- src/restaurnats/restaurnats.resolver.ts
+++ src/restaurnats/restaurnats.resolver.ts
@@ -1,11 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { RestaurnatsEntity } from "./entities/restaurnats.entity";
 
 //return 꼭 필요한건 아니다 , @Resolver() 이렇게 써도 무방함
 @Resolver(of => RestaurnatsEntity)
 export class RestaurnatsResolver {
-  @Query(returns => RestaurnatsEntity)
-  myRestaurnatsEntity() {
-    return true;
+  //graphql 과 typescript 가 타입을 정의하는데 조금씩 차이가 있음
+  @Query(returns => [RestaurnatsEntity])
+  //arg 추가하는 방법
+  myRestaurnatsEntity(@Args('veganOnly') veganOnly: boolean): RestaurnatsEntity[] {
+    console.log('veganOnly', veganOnly);
+    return [];
   }
 }
